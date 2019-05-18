5a9ce55d2b472e5a7714b32bb9b0a9b188bb33fe
ElimuStuart <stuartelimu@gmail.com>
2019-05-18 09:58:53 +0300

    mobile view working

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
 let menuDiv = document.querySelector(".menu");
+let overlayDiv = document.querySelector(".overlay");
+let navLinks = document.querySelectorAll(".nav a");
 
 function toggleActive(){
     menuDiv.classList.toggle("active");
+    overlayDiv.classList.toggle("menu-open");
+}
+
+function removeActive(){
+    overlayDiv.classList.remove("menu-open");
+    menuDiv.classList.remove("active");
 }
 
 menuDiv.addEventListener("click", toggleActive);
+
+for(let i = 0; i < navLinks.length; i++){
+    navLinks[i].addEventListener("click", removeActive);
+}
